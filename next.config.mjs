5d539ac26b4c8ed8935d45123a237c3471474f1b
Steve Gray <heystevegray.dev@gmail.com>
2025-03-21 21:51:24 -0400

    Docker

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,6 @@
 import { withContentlayer } from 'next-contentlayer2'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = { reactStrictMode: true, swcMinify: true, output: 'standalone' }
 
 export default withContentlayer(nextConfig)
